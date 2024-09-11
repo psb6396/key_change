20fd7986be698cbcaa746af7a869f8c5434ccba9
psb6396 <psb6396@naver.com>
2024-09-11 13:16:19 +0900

    ㅁㄴㅇㄹ

--- asdf.py
+++ asdf.py
@@ -1,6 +1,21 @@
 import keyboard
+import time
 
-# while True:
-#     try:
+start_time = None
+duration = 3
+function_have_been_executed = False
 
-keyboard.on_press_key('ctrl', lambda _: print("Key 'ctrl' pressed"))
+def change_key_option():
+    pass
+
+while True:
+    if keyboard.is_pressed('ctrl'):
+        if start_time is None:
+            start_time = time.time()  # Start timing when Ctrl is first pressed
+        elif time.time() - start_time >= duration:
+            print(f"'Ctrl' has been held for {duration} seconds!")
+    else: # ctrl 버튼에서 손 뗐을 때 함수 실행됨.
+        change_key_option() 
+        print('함수실행됨.')
+        start_time = None  # Reset if Ctrl is released
+    time.sleep(0.5)  # Check every 500ms
