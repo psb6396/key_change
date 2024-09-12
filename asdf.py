0de715226686935604a1b6fe6f82e116ed80bc02
psb6396 <psb6396@naver.com>
2024-09-12 18:00:36 +0900

    ㅁㄴㅇㄹ

--- asdf.py
+++ asdf.py
@@ -9,15 +9,15 @@ def change_key_option():
     pass
 
 while True:
-    if keyboard.is_pressed('ctrl'):
+    if keyboard.is_pressed('ctrl') and function_have_been_executed == False:
         if start_time is None:
             start_time = time.time()  # Start timing when Ctrl is first pressed
         elif time.time() - start_time >= duration:
-            print(f"'Ctrl' has been held for {duration} seconds!")
-            if(keyboard.is_pressed('ctrl') == False):
-                change_key_option()
-                print('function have beeen excuted.')
+            print(f"'Ctrl' 버튼이 {time.time() - start_time} 초 동안 눌려짐.")
+
+            function_have_been_executed = True
     elif(keyboard.is_pressed('ctrl') == False):
-        print('Ctrl is released.')
+        print('.')
         start_time = None  # Reset if Ctrl is released
+        function_have_been_executed = False
     time.sleep(0.5)  # Check every 500ms
